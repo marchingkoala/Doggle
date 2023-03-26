a60c0e8a353c20fc7cead0e717925f884ed7b641
Yijin Ro <yri.carmine@gmail.com>
2023-03-26 13:32:27 -0400

    set up initial frame for the frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import Quiz from './Quiz';
 
 /**
  * This is the entry point for all of our react stuff
  */
 const App = () => {
     return (
+      <div>
+        <h1>DOGGLE!</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/quiz">Quiz</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/quiz" element={<Quiz />}></Route>
+        </Routes>
         <div>
-            <h1>Welcome to dsfghjkl, Good luck!</h1>
-            <Link to="/"><button>Home</button></Link>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-            </Routes>
+            <h2>THIS IS WHERE FOOTER WILL GO</h2>
         </div>
+      </div>
     );
 };
 
