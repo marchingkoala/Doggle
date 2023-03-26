8c2b162b34eff94cd0a8d4cf8491a6b13e252cd0
Yijin Ro <yri.carmine@gmail.com>
2023-03-26 13:33:08 -0400

    creating components for future use

---
+++ src/components/Quiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quiz = () => {
+    return (
+        <div>
+            <h1>QUIZ!!</h1>
+            <p>Guess the breed!</p>
+        </div>
+    );
+};
+
+export default Quiz;
