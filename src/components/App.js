c532d538a7216778f7a02b5390e42c882ad64ee5
Yijin Ro <yri.carmine@gmail.com>
2023-04-01 14:25:47 -0400

    axios call to call all pups data. used prop to pass down data to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Quiz from './Quiz';
+import axios from 'axios';
 
-/**
- * This is the entry point for all of our react stuff
- */
+
 const App = () => {
+
+    let [pups, setPups] = useState([]);
+    let [total, setTotal] = useState(0);
+    let [loading, setLoading] = useState(true);
+
+    const pupHandler = async () => {
+      let { data } = await axios.get("/api/pups");
+      setPups(data);
+      setTotal(data.length);
+      setLoading(false);
+      console.log(data);
+    };
+
+    useEffect(() => {
+      pupHandler();
+    }, []);
+
+    if (loading) {
+      return <div>loading</div>;
+    }
+
     return (
       <div>
         <h1>DOGGLE!</h1>
@@ -15,11 +35,11 @@ const App = () => {
           <Link to="/quiz">Quiz</Link>
         </nav>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/quiz" element={<Quiz />}></Route>
+          <Route path="/" element={<Home loading={loading} />}></Route>
+          <Route path="/quiz" element={<Quiz total={total} />}></Route>
         </Routes>
         <div>
-            <h2>THIS IS WHERE FOOTER WILL GO</h2>
+          <h2>THIS IS WHERE FOOTER WILL GO</h2>
         </div>
       </div>
     );
