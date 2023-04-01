f1d7fce6ecb24d4ba4366cbbe0131939e3c97653
Yijin Ro <yri.carmine@gmail.com>
2023-04-01 14:26:36 -0400

    setting up structure for quiz. tested calling random dog data

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './quiz.css';
+const Quiz = ({total}) => {
 
-const Quiz = () => {
+    let randomNum = Math.floor(Math.random() * total)
+    let [quizPup, setQuizPup] = useState({});
+    let [loading, setLoading] = useState(true);
+    let [selectedAnswer, setSelectedAnswer] = useState("");
+
+    const quizPupHandler = async () =>{
+      const pup = await axios.get(`/api/pups/${randomNum}`)
+      setLoading(false)
+      setQuizPup(pup.data)
+      console.log(pup.data);
+    }
+
+    const quizHandler = (event) => {
+      // this allows user to check the box and then un-check it
+      setSelectedAnswer(selectedAnswer === event.target.value ? "": event.target.value);
+    }
+
+    const handerSubmit = (event) =>{
+      event.preventDefault();
+      console.log(selectedAnswer);
+    }
+
+    useEffect(()=>{
+      quizPupHandler();
+    }, [])
+
+    if(loading){ return(<div>loading..</div>)}
     return (
-        <div>
-            <h1>QUIZ!!</h1>
-            <p>Guess the breed!</p>
+      <div>
+        <h2>QUIZ!!</h2>
+        <p>Guess the breed!</p>
+        <img
+          src={quizPup.image}
+          alt="image of mixed breed dog"
+          className="pupImage"
+        />
+        <p className="pupName">Name: {quizPup.name}</p>
+        <p className="pupDescription">"{quizPup.description}"</p>
+        <div className="multipleChoices">
+          <form onSubmit={handerSubmit}>
+            <h2>Take your pick!</h2>
+            {quizPup.choices.map((breed, index) => {
+              return (
+                <div key={index}>
+                  <label className="checkboxOptionLabel">
+                    <input
+                      type="checkbox"
+                      name="answer"
+                      value={breed}
+                      checked={selectedAnswer === breed}
+                      onChange={quizHandler}
+                    />
+                    {breed}
+                  </label>
+                </div>
+              );
+            })}
+            <button type="submit">I made my choice</button>
+          </form>
         </div>
+      </div>
     );
 };
 
