bc32822b379c7416debd863483a46523b70072da
Yijin Ro <yri.carmine@gmail.com>
2023-04-02 15:44:54 -0400

    given props are working. tested and true

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom'
 
 const Result = ({ quizPup, selectedAnswer }) => {
+  let [loading, setLoading] = useState(true);
+  let [useAnswer, setUserAnswer] = useState("");
+
+  const setAnswers = () => {
+    setUserAnswer(selectedAnswer);
+    console.log(quizPup);
+    console.log(selectedAnswer);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    setAnswers();
+  }, []);
+
+  if (loading) {
+    return <div>loading..</div>;
+  }
+
   return (
     <div>
       <h1>RESULT GOES HERE</h1>
+      {/* <p>{quizPup.name}</p>
+      <p>
+        Your answer is: {useAnswer === quizPup.breed ? "correct" : "incorrect"}
+      </p>
+      <img
+        src={quizPup.image}
+        alt="right breed of dog"
+        width="500"
+        height="auto"
+      />
+      <p className="pupDescript">{quizPup.descript}</p>
+      <button onClick={() => window.location.reload()}>Play Again!</button> */}
     </div>
   );
 };
