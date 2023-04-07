f5cff9de1b46b196dd77a16c2458489e86a34c60
Yijin Ro <yri.carmine@gmail.com>
2023-04-07 16:04:39 -0400

    connecting local db to railway

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
+const DB_URL = "postgres://localhost:5432/dogs";
 
-const db = new Sequelize(
-    'postgres://localhost:5432/dogs',
-    { logging: false }
-);
+const db = new Sequelize(process.env.DATABASE_URL || DB_URL, { logging: false });
+
+
 
 module.exports = db;
