eff33f872f7a39228cb2a36944b55eecc8ceecaf
Yijin Ro <yri.carmine@gmail.com>
2023-04-09 13:29:02 -0400

    created components for clean coding

---
+++ src/components/Home_Second_Section/Second_section.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Second_section = () => {
+    return (
+      <div className="home_second_section">
+        <p>
+          "Everybody should have a shelter dog. It's good for the soul." – Paul
+          Shaffer, Canadian musician, composer, actor, author, and comedian
+        </p>
+      </div>
+    );
+};
+
+export default Second_section;
---
+++ src/components/Home_Third_Section/Third_section.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Third_section = () => {
+    return (
+      <div>
+        <p>You can't buy unconditional love but you can rescue one!</p>
+      </div>
+    );
+};
+
+export default Third_section;
