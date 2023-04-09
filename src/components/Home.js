490b1cb18dfc6c734f91302ff340e8b62394dbb0
Yijin Ro <yri.carmine@gmail.com>
2023-04-09 13:28:05 -0400

    added useNavigate to enable button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,39 @@
 import React from 'react';
 import './home.css'
+import { useNavigate } from 'react-router-dom';
+import Second_section from './Home_Second_Section/Second_section';
+import Third_section from './Home_Third_Section/Third_section';
 
 const Home = ({loading}) => {
+
+    const navigate = useNavigate();
 
     if (loading) {
       return <div>loading</div>;
     }
 
+    const navigator = () => {
+      navigate("/quiz")
+    }
+
     return (
       <div>
-          <div className="home_first_section">
-            <div className='first_section_description'>
-              <h2>So you think you know dogs?</h2>
-              <button>Take the Quiz</button>
-            </div>
-            <img
-              src="/Home_IMG/Targle_front.png"
-              width="250px"
-              height="auto"
-              alt="photo of a very cute jindo dog with his tongue out"
-            />
+        <div className="home_first_section">
+          <div className="first_section_description">
+            <h2>So you think you know dogs?</h2>
+            <button onClick={navigator}>Take the Quiz</button>
           </div>
-
-          <div className="home_second_section">
-            <p>So you think you know dogs?</p>
-          </div>
-
-        <div>
-          <p>You can't buy love but you can rescue love!</p>
+          <img
+            src="/Home_IMG/Targle_front.png"
+            width="250px"
+            height="auto"
+            alt="photo of a very cute jindo dog with his tongue out"
+          />
         </div>
-        <div>
-          <p>
-            "Everybody should have a shelter dog. It's good for the soul." –
-            Paul Shaffer, Canadian musician, composer, actor, author, and
-            comedian
-          </p>
-        </div>
+
+        <Second_section />
+        <Third_section />
+
       </div>
     );
 };
