8741cf56396c918e5e5419c20a070b9e7c6f78ed
Yijin Ro <yri.carmine@gmail.com>
2023-06-30 15:00:02 -0400

    ok one more try on the db railway

--- server/db/db.js
+++ server/db/db.js
@@ -4,10 +4,7 @@ const config = {
 }
 const DB_URL = "postgres://localhost:5432/dogs";
 
-// const db = new Sequelize(process.env.DATABASE_URL, config);
-const db = new Sequelize(
-  "postgresql://postgres:RyTimzUNzgM7kF5Xrkgo@containers-us-west-32.railway.app:5840/railway",
-config
-);
+const db = new Sequelize(process.env.DATABASE_URL, config);
+
 
 module.exports = db;
