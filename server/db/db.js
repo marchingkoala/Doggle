7b6c418c27b8488ff9f7cb8a1f82538d12786629
Yijin Ro <yri.carmine@gmail.com>
2023-06-30 14:44:15 -0400

    final touch on the db

--- server/db/db.js
+++ server/db/db.js
@@ -2,15 +2,9 @@ const Sequelize = require("sequelize");
 const config = {
     logging: false,
 }
-const DB_URL = "postgres://localhost:5432/dogs";
+// const DB_URL = "postgres://localhost:5432/dogs";
 
 const db = new Sequelize(process.env.DATABASE_URL, {logging: false} );
-// const db = new Sequelize(
-//   "postgresql://postgres:RyTimzUNzgM7kF5Xrkgo@containers-us-west-32.railway.app:5840/railway",
-//   {
-//     logging: false,
-//   }
-// );
 
 
 module.exports = db;
