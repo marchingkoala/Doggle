76a577b64839138365b8503cf14c7dcd9fda1edf
Yijin Ro <yri.carmine@gmail.com>
2023-06-30 14:31:54 -0400

    db test

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,10 @@
 const Sequelize = require("sequelize");
+const config = {
+    logging: false,
+}
 const DB_URL = "postgres://localhost:5432/dogs";
 
-const db = new Sequelize(process.env.DATABASE_URL || DB_URL, { logging: false });
+const db = new Sequelize(process.env.DATABASE_URL || DB_URL, config );
 
 
 
