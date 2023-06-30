d2ebe5dfde95629cdd2550a38da32d3a32c122a8
Yijin Ro <yri.carmine@gmail.com>
2023-06-30 14:34:55 -0400

    db test number 2

--- server/db/db.js
+++ server/db/db.js
@@ -4,8 +4,10 @@ const config = {
 }
 const DB_URL = "postgres://localhost:5432/dogs";
 
-const db = new Sequelize(process.env.DATABASE_URL || DB_URL, config );
-
+// const db = new Sequelize(process.env.DATABASE_URL || DB_URL, config );
+const db = new Sequelize(
+  "postgresql://postgres:RyTimzUNzgM7kF5Xrkgo@containers-us-west-32.railway.app:5840/railway"
+);
 
 
 module.exports = db;
