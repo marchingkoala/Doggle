4f40100514bba477e681077e7337a5cbc42d2037
Yijin Ro <yri.carmine@gmail.com>
2023-06-30 14:40:40 -0400

    db test with rail url

--- server/db/db.js
+++ server/db/db.js
@@ -4,8 +4,13 @@ const config = {
 }
 const DB_URL = "postgres://localhost:5432/dogs";
 
-const db = new Sequelize(process.env.DATABASE_URL || DB_URL, config );
-
+// const db = new Sequelize(process.env.DATABASE_URL || DB_URL, config );
+const db = new Sequelize(
+  "postgresql://postgres:RyTimzUNzgM7kF5Xrkgo@containers-us-west-32.railway.app:5840/railway",
+  {
+    logging: false,
+  }
+);
 
 
 module.exports = db;
