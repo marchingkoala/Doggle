a209f8fbbe43f1a8664239e2dd4305f13127dc10
Yijin Ro <yri.carmine@gmail.com>
2023-06-30 14:53:13 -0400

    not sure whats wrong but going back to previous commit

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,10 @@ const config = {
 }
 const DB_URL = "postgres://localhost:5432/dogs";
 
-const db = new Sequelize(process.env.DATABASE_URL, {logging: false} );
-
+const db = new Sequelize(process.env.DATABASE_URL, config);
+// const db = new Sequelize(
+//   "postgresql://postgres:RyTimzUNzgM7kF5Xrkgo@containers-us-west-32.railway.app:5840/railway",
+// config
+// );
 
 module.exports = db;
