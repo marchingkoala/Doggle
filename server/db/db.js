9e6785a2f7bf31fb910983874810f3220ffd0af3
Yijin Ro <yri.carmine@gmail.com>
2023-09-10 13:22:34 -0400

    migrating to render from railway

--- server/db/db.js
+++ server/db/db.js
@@ -3,8 +3,12 @@ const config = {
     logging: false,
 }
 const DB_URL = "postgres://localhost:5432/dogs";
+// const TEST_URL =
+//   "postgres://dogs_gj8t_user:Zz3LM6sTnenHn9PYHagLskkWUkdoiMfd@dpg-cjuv7mp5mpss7388oku0-a/dogs_gj8t";
 
-const db = new Sequelize(process.env.DATABASE_URL, config);
+// const db = new Sequelize(process.env.DATABASE_URL, config);
+const db = new Sequelize(DB_URL, config);
+// const db = new Sequelize(TEST_URL, config);
 
 //
 module.exports = db;
